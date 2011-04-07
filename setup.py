7fc9a4446a9be5a95dad10b8ccb737eeb2907490
benzres <yrxinres@gmail.com>
2011-04-07 16:15:39 -0400

    new file:   LICENSE.txt new file:   README.txt new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MethylAnalyzer',
+      version='0.1.0',
+      description='Analyze Methyl-MAPS data',
+      author='Yurong Xin',
+      author_email='xinyuro@pi.cpmc.columbia.edu',
+      url='http://github.com/epigenomics/MethylAnalyzer',
+      packages=['methylanalyzer'],
+      scripts = ['scripts/create_cpg_track.py', 'scripts/create_frag_track.py',
+                 'scripts/create_microarray_track.py', 'scripts/create_wiggle_track.py',
+                 'scripts/filter.py', 'scripts/parse_mates.py', 'scripts/parse_sites.py',
+                 'scripts/run_pipeline.py', 'scripts/score.py'],
+      package_data={'MethylAnalyzer': ['data/*']},
+      requires=['numpy', 'biopython'],
+      license='GPL',
+      )
+
